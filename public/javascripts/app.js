3339f739b3b4b10915292125dc3fe501066fbb2d
Eric Kelly <heroiceric@gmail.com>
2014-08-01 12:03:32 -0400

    Add more comments

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,12 +2,15 @@ $(function() {
   $("#search").autocomplete({
     source: function(request, response) {
       $.ajax({
-        url: '/',
+        url: '/', // this should point to whatever route you're using
         dataType: 'json',
         data: {
-          search: request.term
+          search: request.term // Send the search query parameter in the request
         },
         success: function( data ) {
+          // The results dont come back in the format jquery ui expects
+          // so we make a new array that holds objects in the format
+          // that the autocomplete wants.
           var results = $.map(data, function(movie, index) {
             return { id: movie.id, label: movie.title, value: movie.title };
           });
